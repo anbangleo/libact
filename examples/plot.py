7b9532abce82149951f029aacaee7a1927dad9fd
anbang <anbangleo@gmail.com>
2018-03-14 10:00:13 +0800

    test for plot.py

--- examples/plot.py
+++ examples/plot.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# coding=utf-8
 """
 The script helps guide the users to quickly understand how to use
 libact by going through a simple active learning task with clear
@@ -21,24 +22,25 @@ from libact.models import *
 from libact.query_strategies import *
 from libact.labelers import IdealLabeler
 
-
+#The main part that uses libact is in the run function:
 def run(trn_ds, tst_ds, lbr, model, qs, quota):
     E_in, E_out = [], []
-
     for _ in range(quota):
         # Standard usage of libact objects
-        ask_id = qs.make_query()
+        ask_id = qs.make_query()  #returns the index of the sample that the active learning algorithm wants to query.
         X, _ = zip(*trn_ds.data)
-        lb = lbr.label(X[ask_id])
-        trn_ds.update(ask_id, lb)
-
+        print X
+        print _
+        lb = lbr.label(X[ask_id])#lbr acts as the oracle#returns the label of the given sample answered by oracle.
+        trn_ds.update(ask_id, lb)#updates the unlabeled sample with queried label.
+#A common way of evaluating the performance of active learning algorithm is to plot the learning curve. 
         model.train(trn_ds)
-        E_in = np.append(E_in, 1 - model.score(trn_ds))
-        E_out = np.append(E_out, 1 - model.score(tst_ds))
+        E_in = np.append(E_in, 1 - model.score(trn_ds))#in-sample error
+        E_out = np.append(E_out, 1 - model.score(tst_ds))#out-sample error
 
     return E_in, E_out
 
-
+#First, the data are splitted into training and testing set:
 def split_train_test(dataset_filepath, test_size, n_labeled):
     X, y = import_libsvm_sparse(dataset_filepath).format_sklearn()
 
@@ -48,18 +50,19 @@ def split_train_test(dataset_filepath, test_size, n_labeled):
         [y_train[:n_labeled], [None] * (len(y_train) - n_labeled)]))
     tst_ds = Dataset(X_test, y_test)
     fully_labeled_trn_ds = Dataset(X_train, y_train)
-
     return trn_ds, tst_ds, y_train, fully_labeled_trn_ds
-
+    #trn_ds与fullly相比完全相同，不过fully是全标记，trn_ds只标记了labeled个，格式是[array(标记元素),label]
+    #y_train是fully_labeled的label，list
+    #tst_ds是测试集，1-fully
 
 def main():
     # Specifiy the parameters here:
     # path to your binary classification dataset
     dataset_filepath = os.path.join(
-        os.path.dirname(os.path.realpath(__file__)), 'diabetes.txt')
+        os.path.dirname(os.path.realpath(__file__)), 'diabetes1.txt')
     test_size = 0.33    # the percentage of samples in the dataset that will be
     # randomly selected and assigned to the test set
-    n_labeled = 10      # number of samples that are initially labeled
+    n_labeled = 2      # number of samples that are initially labeled
 
     # Load dataset
     trn_ds, tst_ds, y_train, fully_labeled_trn_ds = \
